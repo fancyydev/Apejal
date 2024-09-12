b61f72850fe9aa0e204177fb77c069fd70e3930a
Veronica Martinez <vero_mtz_08@hotmail.com>
2024-09-11 19:02:45 -0600

    Modificacion en obtenerTecnicos y menuJuntaLocal para obtener los nombres de los municipios

--- Backend/JuntasLocales/obtenerTecnicos.php
+++ Backend/JuntasLocales/obtenerTecnicos.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
 }
 
 // Consulta para obtener los datos del técnico, usuario, y junta local
-$sql = "SELECT t.id_tecnico, u.nombre AS nombre_usuario, u.correo, u.teléfono, j.nombre AS nombre_junta
+$sql = "SELECT t.id_tecnico, u.nombre AS nombre_usuario, u.correo, u.teléfono,t.carga_municipios,t.estatus, j.nombre AS nombre_junta
         FROM tecnico t
         JOIN usuario u ON t.id_usuario = u.id_usuario
         JOIN juntaslocales j ON t.idjuntalocal = j.idjuntalocal";
@@ -29,6 +29,26 @@ $tecnicos = array();
 if ($result->num_rows > 0) {
     // Convertir los datos en un array asociativo
     while ($row = $result->fetch_assoc()) {
+        //array para almacenar los nombres de los municipios
+        $municipios_nombres = array();
+        
+        //Obtener los IDs de los municipios desde la consulta de arriba
+        $ids_municipios = explode(',', $row['carga_municipios']);
+
+        //obtener los nombres de los municipios
+        $municipios_sql = "SELECT nombre FROM municipio WHERE id_municipio IN (" . implode(',', $ids_municipios) . ")";
+        $municipios_result = $conn->query($municipios_sql);
+
+        if ($municipios_result->num_rows > 0) {
+            while ($municipio = $municipios_result->fetch_assoc()) {
+                $municipios_nombres[] = $municipio['nombre'];  // Agregar el nombre del municipio al array
+            }
+        }
+
+        // Agregar los nombres de los municipios a los datos del técnico
+        $row['nombres_municipios'] = $municipios_nombres;
+
+
         $tecnicos[] = $row;
     }
 } else {
